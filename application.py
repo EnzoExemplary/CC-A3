ebfde8efb6c05848c81744b377415b7dcb10ecdd
Marcus Matic <s3703981@student.rmit.edu.au>
2021-06-05 21:33:59 +1000

    Added functionality for login through Lambda API, very basic user page, updated secret key

--- application.py
+++ application.py
@@ -1,18 +1,47 @@
 from flask import Flask, Blueprint, render_template, session, request, flash, url_for, redirect
 from auth import auth
+import requests
+import json
+import os
 
 application = Flask(__name__)
-application.config['SECRET_KEY'] = "+-fY6PsEWdPHZeWxD8T.]P}{K,96Wp6!"
+application.config['SECRET_KEY'] = "\x07/\xc2\xbc\xfd\xb9\xb3<\x1f\xd40\xef3\x92\x01\xeb&\xbd\x8f\xe8r\xc3\xb6"
 application.register_blueprint(auth)
+ENDPOINT_API = "https://iw6n2bfzzd.execute-api.ap-southeast-2.amazonaws.com/prod/"
 
 @application.route('/')
 def root():
+	print(os.urandom(24))
 	return render_template('index.html')	
 	
 @application.route('/search')
 def search():
 	return render_template('search.html')
+	
+@application.route('/user/<username>')
+def user_page(username):
+	
+	data = {'username': username}
+	json_data = json.dumps(data)
+	url = ENDPOINT_API + 'user'
+	response = requests.get(url, data=json_data)
+	response = json.loads(response.text)
+	user = response['user']
+	
+	if not user:
+		user = ''
+	
+	return render_template('user.html', user=user)
+
+#Check username in session, return empty string if no username
+def getUsername():
+	username = ''
+	if 'username' in session:
+		username = session['username']
+		
+	return username
 
 if __name__ == "__main__":
 	application.debug = True
 	application.run()
+	
--- auth.py
+++ auth.py
@@ -1,12 +1,41 @@
 from flask import Flask, Blueprint, render_template, session, redirect, url_for, request, flash
+import requests
+import json
 
 auth = Blueprint('auth', __name__, template_folder='templates')
+ENDPOINT_API = "https://iw6n2bfzzd.execute-api.ap-southeast-2.amazonaws.com/prod/"
 
 @auth.route('/login')
 def login():
 	return render_template('login.html')
 	
+@auth.route('/login', methods=['POST'])
+def login_post():
+	username = request.form.get('username')
+	password = request.form.get('password')
 	
+	data = {'username': username, 'password': password}
+	json_data = json.dumps(data)
+	url = ENDPOINT_API + 'login'
+	response = requests.post(url, data=json_data)
+	response = json.loads(response.text)
+	
+	if(response['match']):
+		session['username'] = username
+		redirect_url = '/user/' + username
+		return redirect(redirect_url)
+	else:
+		flash('Username or password is invalid')
+		return redirect(url_for('auth.login'))
+	
+#Check username in session, return empty string if no username
+def getUsername():
+	username = ''
+	if 'username' in session:
+		username = session['username']
+		
+	return username	
+
 @auth.route('/register')
 def register():
 	return render_template('register.html')
